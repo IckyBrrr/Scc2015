df3cf7ce9fb3db86ebaf0daadfb3aee4081f3840
Christopher Leap <leap.christopher@gmail.com>
2015-11-03 19:50:43 -0700

    Update input system (x y tile)

--- Yavalanchan/src/game/Main.java
+++ Yavalanchan/src/game/Main.java
@@ -3,94 +3,110 @@ package game;
 import java.util.Scanner;
 
 public class Main {
-	
-	public static final byte DNE = -1; // Tile doesn't exist
-	public static final byte EMPTY = 0; // Empty slot
-	public static final byte PLAYER_A = 1; // Player A
-	public static final byte PLAYER_B = 2; // Player B
-	public static final byte NEUTRAL = 3; // Neutral
-	
+
 	public static final int SIZE = 4;
-	
+	public static final int MIN_VAL = 0;
+
 	public static Scanner in;
 	public static Board board;
-	
+
 	public static boolean isRunning;
-	
+	public static int turn;
+
 	public static void main(String[] args) {
 
 		in = new Scanner(System.in);
 		board = new Board(SIZE);
 		isRunning = true;
-		
+		turn = 0;
+
 		render();
-		
+
 		while(isRunning) {
 			update();
 			render();
 		}
 	}
-	
+
 	public static void update() {
-		int x;
-		int y;
+		Integer x = null, y = null;
 		String input;
 		TileState occupant = TileState.EMPTY;
-		boolean canMoveOn;
+		boolean canMoveOn = false;;
+
 		do {
-			do {
-				System.out.println("What X?");
+			//TODO: keep valid values
+			System.out.print("What is your move? (x y tile) : ");
+
+			if(in.hasNextInt()) {
+
 				x = in.nextInt() - 1;
-				if((x < SIZE * 2 - 1) && (x >= 0)) {
-					canMoveOn = true;
-				} else {
-					System.out.println("X doesn't exist");
-					canMoveOn = false;
+
+				if(x < MIN_VAL || x > board.columns.length) {
+					System.out.printf("X needs to be in between %d and %d\n", MIN_VAL + 1, board.columns.length);
+					x = null;
 				}
-			} while(!canMoveOn);
-			
-			canMoveOn = false;
-			
-			do {
-				System.out.println("What Y?");
-				y = in.nextInt() - 1;
-				if((y < board.columns[x].tiles.length) && (y >= 0)) {
-					canMoveOn = true;
-				} else {
-					System.out.println("Y doesn't exist");
-					canMoveOn = false;
-				}
-			} while(!canMoveOn);
-		
-			canMoveOn = false;
-		
-			System.out.println("What Color?");
-			input = in.next();
-			input = input.toLowerCase();
-			if(input.charAt(0) == 'q') {
-				isRunning = false;
-				break;
-			} else if(input.charAt(0) == 'a' || input.charAt(0) == 'w') {
-				occupant = TileState.WHITE;
-			} else if(input.charAt(0) == 'b' || input.charAt(0) == 'b') {
-				occupant = TileState.BLACK;
-			} else if(input.charAt(0) == 'c' || input.charAt(0) == 'n') {
-				occupant = TileState.NEUTRAL;
-			}
-			if(!(occupant == TileState.NEUTRAL || occupant == TileState.WHITE || occupant == TileState.BLACK)) {
-				System.out.println("Invalid color");
 			} else {
-				canMoveOn = board.occupy(occupant, x, y);
+
+				System.out.println("X needs to be an integer");
+				in.next();
+			}
+
+			if(x != null) {
+				if(in.hasNextInt()) {
+
+					y = in.nextInt() - 1;
+
+					if(y < MIN_VAL || y > board.columns[x].tiles.length) {
+
+						System.out.printf("y needs to be in between %d and %d\n", MIN_VAL + 1, board.columns[x].tiles.length);
+						y = null;
+					}
+				} else {
+
+					System.out.println("Y needs to be an integer");
+					in.next();
+				}
+			} else {
+
+				in.next();
+			}
+
+			if(x != null && y != null) {
+				input = in.next();
+				input = input.toLowerCase();
+				System.out.println(input);
+				if(input.charAt(0) == 'q') {
+					isRunning = false;
+					break;
+				} else if(input.charAt(0) == 'a' || input.charAt(0) == 'w') {
+					occupant = TileState.WHITE;
+				} else if(input.charAt(0) == 'b' || input.charAt(0) == 'b') {
+					occupant = TileState.BLACK;
+				} else if(input.charAt(0) == 'c' || input.charAt(0) == 'n') {
+					occupant = TileState.NEUTRAL;
+				}
+				if(!(occupant == TileState.NEUTRAL || occupant == TileState.WHITE || occupant == TileState.BLACK)) {
+					System.out.println("Invalid color");
+				} else {
+					canMoveOn = board.occupy(occupant, x, y);
+				}
+			} else {
+				canMoveOn = false;
+				in.next(); //TODO: account for more inputs than intended
 			}
 		} while(!canMoveOn);
-		int tempx = 4, tempy = 4;
-		System.out.printf("Owner for (%d, %d) is : %s\n", tempx, tempy, board.getTile(tempx, tempy).getOwner().toString());
+
+		//int tempx = 4, tempy = 4;
+		//System.out.printf("Owner for (%d, %d) is : %s\n", tempx, tempy, board.getTile(tempx, tempy).getOwner().toString());
+		turn++;
 	}
-	
+
 	public static void render() {
+		System.out.printf("Turn %d:\n", turn);
 		board.print();
 	}
-	
+
 	public static void print(String str) {
 		System.out.println(str);
 	}
